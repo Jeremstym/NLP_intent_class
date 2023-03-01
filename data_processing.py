9427aa59084bed2b0dfc38df7df08b90ffebb998
Jeremstym <jeremstym@gmail.com>
2023-03-01 20:40:13 +0100

    Add tokenizer premiss

---
+++ data_processing.py
@@ -0,0 +1,55 @@
+# Importing stock ml libraries
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import transformers
+import torch
+from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
+from transformers import BertTokenizer, BertModel, BertConfig
+
+# Sections of config
+
+# Defining some key variables that will be used later on in the training
+MAX_LEN = 200
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+EPOCHS = 1
+LEARNING_RATE = 1e-05
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+
+class CustomDataset(Dataset):
+
+    def __init__(self, dataframe, tokenizer, max_len):
+        self.tokenizer = tokenizer
+        self.data = dataframe
+        self.comment_text = dataframe.comment_text
+        self.targets = self.data.list
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.comment_text)
+
+    def __getitem__(self, index):
+        comment_text = str(self.comment_text[index])
+        comment_text = " ".join(comment_text.split())
+
+        inputs = self.tokenizer.encode_plus(
+            comment_text,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            pad_to_max_length=True,
+            return_token_type_ids=True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        token_type_ids = inputs["token_type_ids"]
+
+
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
+            'targets': torch.tensor(self.targets[index], dtype=torch.float)
+        }
