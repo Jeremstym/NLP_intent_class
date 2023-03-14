58a5b72dd42a7193dded1ce9bd820d2f34b5d840
Jeremstym <jeremstym@gmail.com>
2023-03-14 01:34:50 +0100

    Add ReLU to the baseline

--- baseline.py
+++ baseline.py
@@ -14,11 +14,13 @@ class BERTClass(torch.nn.Module):
             for param in self.l1.parameters():
                 param.requires_grad = False
         self.l2 = torch.nn.Dropout(dropout)
+        self.relu = nn.ReLU()
         self.l3 = torch.nn.Linear(bert_dim, output_dim)
     
     def forward(self, ids, mask, token_type_ids):
         _, output_1= self.l1(ids, attention_mask = mask, token_type_ids = token_type_ids, return_dict=False)
         output_2 = self.l2(output_1)
+        output_2 = self.relu(output_2)
         output = self.l3(output_2)
         return output
 
