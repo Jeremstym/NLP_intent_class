851e0510c9721e156eeee4e592331f7c21eb0d5d
Jeremstym <jeremstym@gmail.com>
2023-03-18 01:31:35 +0100

    Keep only one baseline

--- baseline.py
+++ baseline.py
@@ -18,38 +18,8 @@ class BERTClass(torch.nn.Module):
         self.l3 = torch.nn.Linear(bert_dim, output_dim)
     
     def forward(self, ids, mask, token_type_ids):
-        _, output_1= self.l1(ids, attention_mask = mask, token_type_ids = token_type_ids, return_dict=False)
+        _, output_1= self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids, return_dict=False)
         output_2 = self.l2(output_1)
         output_2 = self.relu(output_2)
         output = self.l3(output_2)
         return output
-
-class BertLinear(nn.Module):
-    """Linear layer after a BERT layer."""
-
-    def __init__(self, bert_type: str, frozen: bool, linear_dim: int, name: str = None) -> None:
-        super().__init__()
-        self.bert_type = bert_type
-        self.frozen = frozen
-        self.linear_dim = linear_dim
-        self.name = name or f"{'frozen_' if frozen else ''}{bert_type}_linear_{linear_dim}"
-        if bert_type == "bert":
-            self.bert = BertModel.from_pretrained("bert-base-uncased")
-        if frozen:
-            for param in self.bert.parameters():
-                param.requires_grad = False
-        self.bert_dim = self.bert.config.hidden_size
-        if linear_dim > 0:
-            self.linear = nn.Linear(self.bert_dim, linear_dim)
-
-    def forward(self, input_ids: torch.Tensor) -> torch.Tensor:
-        pooled_output = self.bert(
-            input_ids=input_ids, attention_mask=(input_ids != 0).long(), return_dict=True
-        )[
-            "pooler_output"
-        ]  # (batch_size, nb_int, bert_dim)
-
-        if self.linear_dim > 0:
-            pooled_output = self.linear(pooled_output)
-
-        return pooled_output
